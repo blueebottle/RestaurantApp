510354b41e98e80b16979f8289fa6e16fd17197e
blueebottle <43521498+blueebottle@users.noreply.github.com>
2023-01-12 16:24:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
+import { LocationContextProvider } from "./src/services/location/location.context";
+import { Navigation } from "./src/infrastructure/navigation";
+import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+export default function App() {
+  return (
+    <>
+      <AuthenticationContextProvider>
+        <FavouritesContextProvider>
+          <LocationContextProvider>
+            <RestaurantsContextProvider>
+              <Navigation />
+            </RestaurantsContextProvider>
+          </LocationContextProvider>
+        </FavouritesContextProvider>
+      </AuthenticationContextProvider>
+
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
